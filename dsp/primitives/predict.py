11fda0d0e28e892507515926175f7ee762adbb95
Dhar Rawal <114010652+drawal1@users.noreply.github.com>
2024-06-05 08:31:45 -0500

    fix(extend generation): Fixed error causing incorrect duplicate generations
    Previous fix was incorrect. The correct fix is to only check the output fields to see if they are empty, instead of all fields

--- dsp/primitives/predict.py
+++ dsp/primitives/predict.py
@@ -117,18 +117,12 @@ def _generate(template: Template, **kwargs) -> Callable:
 
         finished_completions = []
         for completion in completions:
-            extend_gen = False
-            for key in field_names:
-                if key not in completion:
-                    extend_gen = True
-                    break
-
-            if extend_gen:
-                finished_completions.append(
-                    extend_generation(completion, field_names, stage, max_depth, original_example),
-                )
-            else:
+            if all((completion.get(key, "") != "") for key in field_names if key not in example):
                 finished_completions.append(completion)
+                continue
+            finished_completions.append(
+                extend_generation(completion, field_names, stage, max_depth, original_example),
+            )
 
         completions = Completions(finished_completions, template=template)
         example = example.copy(completions=completions)
