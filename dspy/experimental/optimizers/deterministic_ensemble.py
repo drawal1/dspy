60a93baf8327c3564ff9d71c3bcddc677a45d769
Dhar Rawal <114010652+drawal1@users.noreply.github.com>
2024-06-05 20:20:02 +0000

    feat(ensemble): added deterministic ensemble

---
+++ dspy/experimental/optimizers/deterministic_ensemble.py
@@ -0,0 +1,231 @@
+"""Ensemble optimizer and ensembled program."""
+import os
+import pickle
+import shutil
+from contextlib import suppress
+from pathlib import Path
+from typing import Any, Optional
+
+import numpy as np
+
+import dsp
+import dspy
+from dspy.teleprompt.teleprompt import Teleprompter
+
+
+class EnsembledProgram(dspy.Module):
+    """An ensemble of candidate programs.
+
+    TODO: The EnsembledProgram should imitate the structure of the individual programs.
+    (IF they are all compatible).
+    This allows compiling with an ensemble program as a (singular) teacher.
+    Basically the top majority-compatible trace will end up being used,
+    if dspy.majority is the reduce_fn.
+    """
+
+    def __init__(self, reduce_fn=None, size=None, deterministic=False, min_acceptable_score: float = -1.0):
+        if not deterministic:
+            if not reduce_fn:
+                raise ValueError("Set deterministic to True or provide a reduce_fn to reduce the outputs.")
+            if size is not None and size == 0:
+                raise ValueError("If reduce function is specified, ensemble size must be greater than 0.")
+
+        super().__init__()
+        self.reduce_fn = reduce_fn
+        self.size = size
+        self.deterministic = deterministic
+        self.min_acceptable_score = min_acceptable_score
+        self.programs = None
+        # For deterministic ensemble
+        self.exampleset_vectors: Any = None
+        self.best_pgm_indices: Any = None
+        self.best_pgm_scores: Any = None
+        # For SentenceTransformersVectorizer
+        self.vectorizer = dsp.SentenceTransformersVectorizer()
+
+    def forward(self, **kwargs) -> dspy.Prediction:
+        """Runs the ensemble of programs and returns the outputs."""
+        outputs = []
+
+        if self.deterministic:
+            serialized_input = " | ".join([f"{key}: {value}" for key, value in kwargs.items()])  # pylint: disable=protected-access
+            input_example_vector = self.vectorizer([serialized_input]).astype(np.float32)[0]
+            scores = np.dot(self.exampleset_vectors, input_example_vector.T).squeeze()
+
+            k = 1  # get just the top matching example
+            nearest_example_idx = (scores.argsort()[-k:][::-1])[0]
+
+            best_program_score = self.best_pgm_scores[nearest_example_idx]
+            if self.min_acceptable_score > 0.0 and best_program_score < self.min_acceptable_score:
+                best_matching_program_index = -1
+            else:
+                # get the best matching program index for the nearest example
+                best_matching_program_index = self.best_pgm_indices[nearest_example_idx]
+
+            if best_matching_program_index != -1:
+                outputs = [self.programs[best_matching_program_index](**kwargs)]
+
+        if not outputs:
+            size = self.size if self.size and self.size < len(self.programs) else None
+            programs = self.programs[:size] if size else self.programs
+            outputs = [prog(**kwargs) for prog in programs]
+
+        if len(outputs) == 1:
+            return outputs[0]
+
+        if self.reduce_fn:
+            return self.reduce_fn(outputs)
+
+        raise ValueError("Set deterministic to True or provide a reduce_fn to reduce the outputs.")
+
+    def save_folder(self, folder_path: str) -> None:
+        """Saves the ensembled program to the folder_path."""
+        # if the file or folder with the same path exists delete the existing file/folder
+        with suppress(FileNotFoundError):
+            shutil.rmtree(folder_path)
+
+        # create the folder using path.mkdir(parents=True)
+        Path(folder_path).mkdir(parents=True)
+
+        # create a file called deterministic.pkl and serialize self.best_pgm_indices, self.best_pgm_scores,
+        # self.exampleset_vectors
+        if self.deterministic:
+            deterministic_dict = {
+                "exampleset_vectors": self.exampleset_vectors,
+                "best_pgm_indices": self.best_pgm_indices,
+                "best_pgm_scores": self.best_pgm_scores,
+            }
+            # serialize the deterministic_dict to a file called deterministic.pkl in the folder_path
+            # using python pickle format
+            with Path.open(Path(folder_path) / "deterministic.pkl", "wb", encoding="UTF-8") as file:
+                pickle.dump(deterministic_dict, file)
+
+        # now loop over all the programs in the compiled model and
+        # save them to a file called 1.json, 2.json, 3.json, etc.
+        for i, program in enumerate(self.programs):
+            program.save(Path(folder_path) / f"{i}.json")
+
+    def load_folder(self, folder_path: str, program_class: type, activate_assertions: bool, *args: Any) -> None:
+        """Loads the ensembled program from the folder_path."""
+        # get the number of .json files in the folder_path
+        num_of_programs = len([f for f in os.listdir(folder_path) if f.endswith(".json")])
+
+        self.programs = []
+        for i in range(num_of_programs):
+            # instantiate the program class
+            program = program_class(*args).activate_assertions() if activate_assertions else program_class(*args)
+            program.load(Path(folder_path) / f"{i}.json")
+            self.programs.append(program)
+
+        # load the deterministic.pkl file if it exists
+        if self.deterministic:
+            try:
+                with Path.open(Path(folder_path) / "deterministic.pkl", "rb", encoding="UTF-8") as file:
+                    deterministic_dict = pickle.load(file)
+                    self.exampleset_vectors = deterministic_dict["exampleset_vectors"]
+                    self.best_pgm_indices = deterministic_dict["best_pgm_indices"]
+                    self.best_pgm_scores = deterministic_dict["best_pgm_scores"]
+            except FileNotFoundError as exc:
+                raise FileNotFoundError(f"deterministic.pkl file not found in the folder path {folder_path}.") from exc
+
+
+@staticmethod
+def default_example_serialization_func(example: Any) -> str:
+    """Default serialization function for examples."""
+    return " | ".join([f"{key}: {value}" for key, value in example.items() if key in example._input_keys])  # pylint: disable=protected-access
+
+
+class Ensemble(Teleprompter):
+    """An ensemble optimizer that optimizes candidate programs."""
+
+    def __init__(
+        self,
+        *,
+        reduce_fn=None,
+        size=None,
+        deterministic=False,
+        min_acceptable_score: float = -1.0,
+        metric_func=None,
+        example_serialization_func=default_example_serialization_func,
+    ):
+        """A common reduce_fn is dspy.majority."""
+        self.reduce_fn = reduce_fn
+        self.size = size
+        self.deterministic = deterministic
+        self.min_acceptable_score = min_acceptable_score
+        self.metric_func = metric_func
+        self.example_serialization_func = example_serialization_func.__func__
+
+        if self.deterministic:
+            if not self.metric_func:
+                raise ValueError("metric_func must be provided for deterministic ensemble.")
+            if not self.example_serialization_func:
+                raise ValueError("example_serialization_func must be provided for deterministic ensemble.")
+
+    def process_score_matrix(self, program_subscores_lists: list[list[float]]) -> tuple[list[int], list[float]]:
+        """Process the score matrix to find the row indices with the maximum value for each column."""
+        # Step 1: Put all values in a matrix
+        matrix = np.array(program_subscores_lists)
+
+        # Step 2: calculate the max_scores
+        max_scores = np.max(matrix, axis=0)
+        # print(max_scores)
+
+        # Step 3: For each column, get a row index representing a row with the maximum score for that column
+        best_pgm_indices = [np.where(matrix[:, col] >= max_scores[col])[0][0] for col in range(matrix.shape[1])]
+
+        # For each column, create a list of scores corresponding to the pgm_indices for that column
+        best_pgm_scores = [matrix[row, col] for col, row in enumerate(best_pgm_indices)]
+
+        # print(f"Best program indices: {best_pgm_indices}")
+        # print(f"Best program scores: {best_pgm_scores}")
+        return best_pgm_indices, best_pgm_scores
+
+    def optimize(
+        self,
+        candidate_programs: list[Any],
+        trainset: Optional[Any],
+        valset: Optional[Any] = None,
+    ) -> EnsembledProgram:
+        """Compiles the ensemble of programs."""
+        ensembled_program = EnsembledProgram(self.reduce_fn, self.size, self.deterministic, self.min_acceptable_score)
+        ensembled_program.programs = [x[-1] for x in candidate_programs]
+
+        if self.deterministic:
+            if not trainset and not valset:
+                raise ValueError("Either trainset or valset must be provided for compiling deterministic ensemble.")
+            exampleset = valset if valset else trainset
+
+            program_subscores_lists = [pgm[1] for pgm in candidate_programs]
+            ensembled_program.best_pgm_indices, ensembled_program.best_pgm_scores = self.process_score_matrix(
+                program_subscores_lists,
+            )
+            if len(ensembled_program.best_pgm_indices) != len(exampleset):
+                raise ValueError(
+                    "The number of examples in the exampleset must match the number of examples in the score matrix."
+                    "Did you specify a valset in BootstrapRandomFewShot? If so, provide that valset to this function",
+                )
+
+            vectorizer = dsp.SentenceTransformersVectorizer()
+            serialized_exampleset_inputs = [self.example_serialization_func(example) for example in exampleset]  # pylint: disable=protected-access
+            ensembled_program.exampleset_vectors = vectorizer(serialized_exampleset_inputs).astype(np.float32)
+
+            example_scores = []
+            for example in exampleset:
+                input_example_vector = vectorizer(self.example_serialization_func(example)).astype(np.float32)
+                scores = np.dot(ensembled_program.exampleset_vectors, input_example_vector.T).squeeze()
+
+                k = 1  # get just the top matching example
+                nearest_example_idx = (scores.argsort()[-k:][::-1])[0]
+
+                # get the best matching programs indices for the nearest example
+                best_matching_program_index = ensembled_program.best_pgm_indices[nearest_example_idx]
+                pgm = ensembled_program.programs[best_matching_program_index]
+                example_input = {key: value for key, value in example.items() if key in example._input_keys}  # pylint: disable=protected-access
+                prediction = pgm(**example_input)
+                example_scores.append(self.metric_func(example, prediction, trace=None))
+
+            dspy.logger.info(f"Mean score for each example: {example_scores}")
+            dspy.logger.info(f"Overall score: {np.mean(example_scores)}")
+
+        return ensembled_program
---
+++ dspy/experimental/optimizers/test_deterministic_ensemble.py
@@ -0,0 +1,147 @@
+"""Testing the minority report ensemble with a deterministic flag set to true."""
+import datasets
+
+import dspy
+from dsp.utils.metrics import f1_score
+from dspy.evaluate import Evaluate
+from dspy.experimental.optimizers.deterministic_ensemble import Ensemble
+from dspy.teleprompt import BootstrapFewShotWithRandomSearch
+from dspy.teleprompt.bootstrap import BootstrapFewShot
+
+ds = datasets.load_dataset("b-mc2/sql-create-context")
+
+demos = [dspy.Example(**d).with_inputs("context", "question") for d in ds["train"]]
+train, test = demos[:100], demos[100:200]
+
+OPENAI_API_KEY = input("Enter your OPENAI_API_KEY: ")
+lm = dspy.OpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY, max_tokens=4000)
+dspy.settings.configure(lm=lm)
+
+exact_match = lambda ex, pred, _trace=None: ex.answer == pred.sql_query
+
+program = dspy.TypedPredictor("context, question -> sql_query")
+
+
+evaluator_exactmatch = Evaluate(
+    devset=test,
+    num_threads=30,
+    metric=exact_match,
+    display_progress=False,
+)
+
+print("BASELINE:")
+print(evaluator_exactmatch(program))
+
+compiled_bootstrapfewshot = BootstrapFewShot(
+    metric=exact_match,
+    max_rounds=1,
+).compile(
+    program,
+    trainset=train,
+)
+
+print("BOOTSTRAPFEWSHOT:")
+print(evaluator_exactmatch(compiled_bootstrapfewshot))
+
+NUMOF_CANDIDATE_PROGRAMS = 15
+
+compiled_bootstrapfewshotwithrandomsearch = BootstrapFewShotWithRandomSearch(
+    metric=exact_match,
+    max_bootstrapped_demos=5,
+    max_labeled_demos=5,
+    num_candidate_programs=NUMOF_CANDIDATE_PROGRAMS,
+    num_threads=NUMOF_CANDIDATE_PROGRAMS,
+).compile(
+    program,
+    trainset=train,
+)
+
+teacher_lm = dspy.OpenAI(model="gpt-4-0125-preview", api_key=OPENAI_API_KEY, max_tokens=4000)
+
+compiled_bootstrapfewshotwithrandomsearch_withteacher = BootstrapFewShotWithRandomSearch(
+    metric=exact_match,
+    teacher_settings={"lm": teacher_lm},
+    max_bootstrapped_demos=5,
+    max_labeled_demos=5,
+    num_candidate_programs=NUMOF_CANDIDATE_PROGRAMS,
+    num_threads=NUMOF_CANDIDATE_PROGRAMS,
+).compile(
+    program,
+    teacher=compiled_bootstrapfewshotwithrandomsearch,
+    trainset=train,
+)
+
+print("BOOTSTRAPFEWSHOTWITHRANDOMSEARCH:")
+print(evaluator_exactmatch(compiled_bootstrapfewshotwithrandomsearch))
+
+print("BOOTSTRAPFEWSHOTWITHRANDOMSEARCH_WITHTEACHER:")
+print(evaluator_exactmatch(compiled_bootstrapfewshotwithrandomsearch_withteacher))
+
+print("*************NOW SWITCH TO A METRIC THAT USES THE TRACE FLAG:*************************")
+
+
+def metric_usingtraceflag(ex, pred, trace=None) -> bool:
+    """Return true if the predicted answer is correct."""
+    if trace is None:  # if we're doing evaluation or optimization
+        return f1_score(ex.answer, pred.sql_query)
+
+    return ex.answer == pred.sql_query
+
+
+evaluator_metricusingtraceflag = Evaluate(
+    devset=test,
+    num_threads=30,
+    metric=metric_usingtraceflag,
+    display_progress=False,
+)
+
+compiled_bootstrapfewshotwithrandomsearch = BootstrapFewShotWithRandomSearch(
+    metric=metric_usingtraceflag,
+    max_bootstrapped_demos=5,
+    max_labeled_demos=5,
+    num_candidate_programs=NUMOF_CANDIDATE_PROGRAMS,
+    num_threads=NUMOF_CANDIDATE_PROGRAMS,
+).compile(
+    program,
+    trainset=train,
+)
+
+compiled_bootstrapfewshotwithrandomsearch_withteacher = BootstrapFewShotWithRandomSearch(
+    metric=metric_usingtraceflag,
+    teacher_settings={"lm": teacher_lm},
+    max_bootstrapped_demos=5,
+    max_labeled_demos=5,
+    num_candidate_programs=NUMOF_CANDIDATE_PROGRAMS,
+    num_threads=NUMOF_CANDIDATE_PROGRAMS,
+).compile(
+    program,
+    teacher=compiled_bootstrapfewshotwithrandomsearch,
+    trainset=train,
+)
+
+print("BOOTSTRAPFEWSHOTWITHRANDOMSEARCH:")
+print(evaluator_metricusingtraceflag(compiled_bootstrapfewshotwithrandomsearch))
+
+print("BOOTSTRAPFEWSHOTWITHRANDOMSEARCH_WITHTEACHER:")
+print(evaluator_metricusingtraceflag(compiled_bootstrapfewshotwithrandomsearch_withteacher))
+
+print("*************EVALUATING THE PROGRAMS WITH THE ORIGINAL BINARY METRIC:**************")
+print("FINAL_BOOTSTRAPFEWSHOTWITHRANDOMSEARCH_WITHTEACHER_WITH_ORIGINAL_METRIC:")
+print(evaluator_exactmatch(compiled_bootstrapfewshotwithrandomsearch_withteacher))
+
+compiled_deterministic_ensemble = Ensemble(
+    reduce_fn=dspy.majority,
+    deterministic=True,
+    metric_func=metric_usingtraceflag,
+).optimize(
+    compiled_bootstrapfewshotwithrandomsearch_withteacher.candidate_programs,
+    trainset=train,
+)
+
+print("DETERMINISTIC ENSEMBLE OPTIMIZER:")
+print(evaluator_metricusingtraceflag(compiled_deterministic_ensemble))
+
+print("DETERMINISTIC ENSEMBLE OPTIMIZER WITH SIZE AND MIN ACCEPTABLE SCORE:")
+compiled_deterministic_ensemble.min_acceptable_score = 0.7
+compiled_deterministic_ensemble.size = NUMOF_CANDIDATE_PROGRAMS
+print(evaluator_metricusingtraceflag(compiled_deterministic_ensemble))
